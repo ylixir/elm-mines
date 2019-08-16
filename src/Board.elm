66d62583dde9f0c57a8ec45354237b5fb7bbf5ee
Jon Allen <jon@ylixir.io>
2019-08-16 11:12:48 +0200

    autoformat

--- src/Board.elm
+++ src/Board.elm
@@ -1,131 +1,192 @@
 module Board exposing
-  ( Board
-  , Content(..)
-  , Square
-  , Tile(..)
-  , bomb
-  , empty
-  , maxX
-  , maxY
-  , square
-  , sweep
-  , toggle
-  )
+    ( Board
+    , Content(..)
+    , Square
+    , Tile(..)
+    , bomb
+    , empty
+    , maxX
+    , maxY
+    , square
+    , sweep
+    , toggle
+    )
 
 import Array exposing (Array)
 
-type Tile
-  = Plain
-  | Exposed
-  | Flagged
-  | Question
-type Content
-  = Bomb
-  | Neighbor Int
 
-type alias Square = (Tile ,Content)
+type Tile
+    = Plain
+    | Exposed
+    | Flagged
+    | Question
+
+
+type Content
+    = Bomb
+    | Neighbor Int
+
+
+type alias Square =
+    ( Tile, Content )
+
+
 
 -- we should do an opaque type here, but i'm just going to keep things simple
+
+
 type alias Board =
-  { width: Int
-  , squares: Array Square
-  }
-
-bomb: Int -> Int -> Board -> Board
-bomb x y b = 
-  let
-    n = realIndex x y b
-  in
-    case (Maybe.withDefault (Exposed, Bomb) <| Array.get n b.squares) of
-      (t,Bomb) -> b
-      (t,c) -> { b| squares = (b.squares
-        |> Array.set n (t, Bomb)
-        |> increment (n+b.width-1)
-        |> increment (n+b.width)
-        |> increment (n+b.width+1)
-        |> increment (n+1)
-        |> increment (n-1)
-        |> increment (n-b.width-1)
-        |> increment (n-b.width)
-        |> increment (n-b.width+1)
-        )} 
+    { width : Int
+    , squares : Array Square
+    }
 
 
-empty: Int -> Int -> Board
+bomb : Int -> Int -> Board -> Board
+bomb x y b =
+    let
+        n =
+            realIndex x y b
+    in
+    case Maybe.withDefault ( Exposed, Bomb ) <| Array.get n b.squares of
+        ( t, Bomb ) ->
+            b
+
+        ( t, c ) ->
+            { b
+                | squares =
+                    b.squares
+                        |> Array.set n ( t, Bomb )
+                        |> increment (n + b.width - 1)
+                        |> increment (n + b.width)
+                        |> increment (n + b.width + 1)
+                        |> increment (n + 1)
+                        |> increment (n - 1)
+                        |> increment (n - b.width - 1)
+                        |> increment (n - b.width)
+                        |> increment (n - b.width + 1)
+            }
+
+
+empty : Int -> Int -> Board
 empty w h =
-  { width = w+2
-  , squares = Array.initialize ((w+2)*(h+2)) (\i -> (Plain, Neighbor 0))
-  }
+    { width = w + 2
+    , squares = Array.initialize ((w + 2) * (h + 2)) (\i -> ( Plain, Neighbor 0 ))
+    }
 
-maxX: Board -> Int
-maxX b = b.width - 3
 
-maxY: Board -> Int
-maxY b = (Array.length b.squares) // b.width - 3
+maxX : Board -> Int
+maxX b =
+    b.width - 3
 
-square: Int -> Int -> Board -> Square
+
+maxY : Board -> Int
+maxY b =
+    Array.length b.squares // b.width - 3
+
+
+square : Int -> Int -> Board -> Square
 square x y b =
-  let n = realIndex x y b
-  in Maybe.withDefault (Exposed, Bomb) <| Array.get n b.squares
+    let
+        n =
+            realIndex x y b
+    in
+    Maybe.withDefault ( Exposed, Bomb ) <| Array.get n b.squares
 
-sweep: Int -> Int -> Board -> Board
+
+sweep : Int -> Int -> Board -> Board
 sweep x y b =
-  let
-    n = realIndex x y b
-    (_,c) = Maybe.withDefault (Exposed, Bomb) <| Array.get n b.squares
-  in
-    { b| squares = b.squares |> flood b.width n |> Array.set n (Exposed, c) } 
+    let
+        n =
+            realIndex x y b
+
+        ( _, c ) =
+            Maybe.withDefault ( Exposed, Bomb ) <| Array.get n b.squares
+    in
+    { b | squares = b.squares |> flood b.width n |> Array.set n ( Exposed, c ) }
 
 
-toggle: Int -> Int -> Board -> Board
+toggle : Int -> Int -> Board -> Board
 toggle x y b =
-  let
-    n = realIndex x y b
-    s = case (Maybe.withDefault (Exposed, Bomb) <| Array.get n b.squares) of
-      (Plain,c) -> (Flagged,c)
-      (Flagged,c) -> (Question,c)
-      (Question,c) -> (Plain,c)
-      (Exposed,c) -> (Exposed,c)
-  in
-    { b| squares = Array.set n s b.squares } 
+    let
+        n =
+            realIndex x y b
+
+        s =
+            case Maybe.withDefault ( Exposed, Bomb ) <| Array.get n b.squares of
+                ( Plain, c ) ->
+                    ( Flagged, c )
+
+                ( Flagged, c ) ->
+                    ( Question, c )
+
+                ( Question, c ) ->
+                    ( Plain, c )
+
+                ( Exposed, c ) ->
+                    ( Exposed, c )
+    in
+    { b | squares = Array.set n s b.squares }
+
 
 
 -- not exposed
-
 --note we have no rails here to avoid index overflows
-realIndex: Int -> Int -> Board -> Int
-realIndex x y b = b.width + (y*b.width) + 1 + x
 
-increment: Int -> Array Square -> Array Square
-increment n b = case (Maybe.withDefault (Exposed, Bomb) <| Array.get n b) of
-  (t,Bomb) -> b
-  (t,Neighbor x) -> Array.set n (t, Neighbor (x+1)) b
 
-edge: Int -> Int -> (Array a) -> Bool
+realIndex : Int -> Int -> Board -> Int
+realIndex x y b =
+    b.width + (y * b.width) + 1 + x
+
+
+increment : Int -> Array Square -> Array Square
+increment n b =
+    case Maybe.withDefault ( Exposed, Bomb ) <| Array.get n b of
+        ( t, Bomb ) ->
+            b
+
+        ( t, Neighbor x ) ->
+            Array.set n ( t, Neighbor (x + 1) ) b
+
+
+edge : Int -> Int -> Array a -> Bool
 edge width n b =
-  let
-      left = 0 == (modBy width n)
-      right = (width - 1) == (modBy width n)
-      top = width > n
-      bottom = n + width > (Array.length b)
-  in left || right || top || bottom 
+    let
+        left =
+            0 == modBy width n
 
-flood: Int -> Int -> (Array Square) -> (Array Square)
+        right =
+            (width - 1) == modBy width n
+
+        top =
+            width > n
+
+        bottom =
+            n + width > Array.length b
+    in
+    left || right || top || bottom
+
+
+flood : Int -> Int -> Array Square -> Array Square
 flood width n b =
-  let
-    s = Maybe.withDefault (Exposed, Bomb) <| Array.get n b
-  in
-    if (edge width n b) then
-      b
-    else case s of
-      (Plain,Neighbor 0) ->
+    let
+        s =
+            Maybe.withDefault ( Exposed, Bomb ) <| Array.get n b
+    in
+    if edge width n b then
         b
-          |> Array.set n (Exposed, Tuple.second s)
-          |> flood width (n+width)
-          |> flood width (n-width)
-          |> flood width (n+1)
-          |> flood width (n-1)
-      (Plain,Neighbor _) ->
-        Array.set n (Exposed, Tuple.second s) b
-      (_,_) ->
-        b
+
+    else
+        case s of
+            ( Plain, Neighbor 0 ) ->
+                b
+                    |> Array.set n ( Exposed, Tuple.second s )
+                    |> flood width (n + width)
+                    |> flood width (n - width)
+                    |> flood width (n + 1)
+                    |> flood width (n - 1)
+
+            ( Plain, Neighbor _ ) ->
+                Array.set n ( Exposed, Tuple.second s ) b
+
+            ( _, _ ) ->
+                b
--- src/Main.elm
+++ src/Main.elm
@@ -1,80 +1,117 @@
 --external modules
-import Array exposing (Array)
-import Browser
-import Html exposing (Html, Attribute, button, div, input, span, text)
-import Html.Attributes exposing (style, type_, value)
-import Html.Events exposing (onClick, onInput, preventDefaultOn)
-import Time exposing (Posix, posixToMillis)
-import Random exposing (Seed, initialSeed)
-import Json.Decode as Json
-import Task
+
+
+module Main exposing (Model, Msg(..), Settings, SettingsMsg(..), base, beginner, blank, bombBoard, expert, flagBombCount, init, intermediate, lost, main, newBoard, onRightClick, row, swept, unswept, update, updateSettings, view, viewBoard, viewRow, viewSettings, viewSquare)
 
 --internal modules
-import Board
+
+import Array exposing (Array)
 import Board exposing (Board, Content(..), Square, Tile(..))
+import Browser
+import Html exposing (Attribute, Html, button, div, input, span, text)
+import Html.Attributes exposing (style, type_, value)
+import Html.Events exposing (onClick, onInput, preventDefaultOn)
+import Json.Decode as Json
+import Random exposing (Seed, initialSeed)
+import Task
+import Time exposing (Posix, posixToMillis)
+
 
 main =
-  Browser.element { init = init, update = update, view = view, subscriptions = (\a->Sub.none) }
+    Browser.element { init = init, update = update, view = view, subscriptions = \a -> Sub.none }
+
 
 
 -- MODEL
 
-bombBoard: Int -> Seed -> Board -> Board
-bombBoard bombs seed board =
-  let
-    (x, tmpSeed) = Random.step (Random.int 0 <| Board.maxX board) seed
-    (y, nextSeed) = Random.step (Random.int 0 <| Board.maxY board) tmpSeed
-  in
-    if 0 == bombs then
-      board
-    else case Board.square x y board of 
-      (_, Bomb) -> bombBoard bombs nextSeed board
-      (_, _) -> bombBoard (bombs - 1) nextSeed (Board.bomb x y board)
 
-flagBombCount: (Array Square) -> (Int,Int)
+bombBoard : Int -> Seed -> Board -> Board
+bombBoard bombs seed board =
+    let
+        ( x, tmpSeed ) =
+            Random.step (Random.int 0 <| Board.maxX board) seed
+
+        ( y, nextSeed ) =
+            Random.step (Random.int 0 <| Board.maxY board) tmpSeed
+    in
+    if 0 == bombs then
+        board
+
+    else
+        case Board.square x y board of
+            ( _, Bomb ) ->
+                bombBoard bombs nextSeed board
+
+            ( _, _ ) ->
+                bombBoard (bombs - 1) nextSeed (Board.bomb x y board)
+
+
+flagBombCount : Array Square -> ( Int, Int )
 flagBombCount a =
-  let
-    counter: Square -> (Int, Int) -> (Int, Int)
-    counter s (f,b) =
-      case s of
-        (Flagged,Bomb)-> (f+1,b+1)
-        (_,Bomb) -> (f,b+1)
-        (Flagged,_) -> (f+1,b)
-        (_,_) -> (f,b)
-  in Array.foldl counter (0,0) a
+    let
+        counter : Square -> ( Int, Int ) -> ( Int, Int )
+        counter s ( f, b ) =
+            case s of
+                ( Flagged, Bomb ) ->
+                    ( f + 1, b + 1 )
+
+                ( _, Bomb ) ->
+                    ( f, b + 1 )
+
+                ( Flagged, _ ) ->
+                    ( f + 1, b )
+
+                ( _, _ ) ->
+                    ( f, b )
+    in
+    Array.foldl counter ( 0, 0 ) a
+
 
 type alias Settings =
-  { seed: Int
-  , width: Int
-  , height: Int
-  , bombs: Int
-  }
+    { seed : Int
+    , width : Int
+    , height : Int
+    , bombs : Int
+    }
 
-beginner: Int -> Settings
-beginner s = { seed = s, width = 9, height = 9, bombs = 10}
-intermediate: Int -> Settings
-intermediate s = { seed = s, width = 16, height = 16, bombs = 40}
-expert: Int -> Settings
-expert s = { seed = s, width = 30, height = 16, bombs = 99}
 
-newBoard: Settings -> Board
+beginner : Int -> Settings
+beginner s =
+    { seed = s, width = 9, height = 9, bombs = 10 }
+
+
+intermediate : Int -> Settings
+intermediate s =
+    { seed = s, width = 16, height = 16, bombs = 40 }
+
+
+expert : Int -> Settings
+expert s =
+    { seed = s, width = 30, height = 16, bombs = 99 }
+
+
+newBoard : Settings -> Board
 newBoard g =
-  let
-    empty = Board.empty g.width g.height
-  in
+    let
+        empty =
+            Board.empty g.width g.height
+    in
     bombBoard g.bombs (initialSeed g.seed) empty
 
-type alias Model =
-  { settings: Settings
-  , board: Board
-  , finished: Bool
-  }
 
-init : () -> (Model, Cmd Msg)
+type alias Model =
+    { settings : Settings
+    , board : Board
+    , finished : Bool
+    }
+
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-  let
-    g = beginner 0
-  in
+    let
+        g =
+            beginner 0
+    in
     ( { settings = g
       , board = newBoard g
       , finished = False
@@ -83,158 +120,255 @@ init _ =
     )
 
 
+
 -- UPDATE
 
+
 type Msg
-  = Lose Int Int
-  | Reset
-  | SeedTime Posix
-  | Set (Int -> Settings)
-  | Sweep Int Int
-  | Toggle Int Int
-  | Change SettingsMsg
+    = Lose Int Int
+    | Reset
+    | SeedTime Posix
+    | Set (Int -> Settings)
+    | Sweep Int Int
+    | Toggle Int Int
+    | Change SettingsMsg
+
 
 type SettingsMsg
-  = Bombs String
-  | Height String
-  | Seed String
-  | TimeRequest
-  | Width String
+    = Bombs String
+    | Height String
+    | Seed String
+    | TimeRequest
+    | Width String
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg m =
-  let g = m.settings in
-  case (m.finished, msg) of
-    (_,SeedTime t) -> 
-      let settings = {g|seed=(posixToMillis t)}
-      in ({board=(newBoard settings), finished=False, settings=settings}, Cmd.none)
-    (_,Set s) ->
-      let settings = s g.seed
-      in ({board=(newBoard settings), finished=False, settings=settings}, Cmd.none)
-    (_,Change s) ->
-      let (ns,c) = updateSettings s g
-      in ({ m | settings = ns } , c)
-    (_,Reset) -> ({ m | board = newBoard m.settings, finished = False }, Cmd.none)
-    (True,_) -> (m, Cmd.none)
-    (_,Sweep x y) -> ({ m | board = Board.sweep x y m.board }, Cmd.none)
-    (_,Lose x y) -> ({ m | finished=True, board = Board.sweep x y m.board }, Cmd.none)
-    (_,Toggle x y) -> ({ m | board = Board.toggle x y m.board }, Cmd.none)
+    let
+        g =
+            m.settings
+    in
+    case ( m.finished, msg ) of
+        ( _, SeedTime t ) ->
+            let
+                settings =
+                    { g | seed = posixToMillis t }
+            in
+            ( { board = newBoard settings, finished = False, settings = settings }, Cmd.none )
 
-updateSettings: SettingsMsg -> Settings -> (Settings, Cmd Msg)
+        ( _, Set s ) ->
+            let
+                settings =
+                    s g.seed
+            in
+            ( { board = newBoard settings, finished = False, settings = settings }, Cmd.none )
+
+        ( _, Change s ) ->
+            let
+                ( ns, c ) =
+                    updateSettings s g
+            in
+            ( { m | settings = ns }, c )
+
+        ( _, Reset ) ->
+            ( { m | board = newBoard m.settings, finished = False }, Cmd.none )
+
+        ( True, _ ) ->
+            ( m, Cmd.none )
+
+        ( _, Sweep x y ) ->
+            ( { m | board = Board.sweep x y m.board }, Cmd.none )
+
+        ( _, Lose x y ) ->
+            ( { m | finished = True, board = Board.sweep x y m.board }, Cmd.none )
+
+        ( _, Toggle x y ) ->
+            ( { m | board = Board.toggle x y m.board }, Cmd.none )
+
+
+updateSettings : SettingsMsg -> Settings -> ( Settings, Cmd Msg )
 updateSettings msg settings =
-  let z t = Maybe.withDefault 0 <| String.toInt t
-  in case msg of
-    Bombs t ->  ({ settings | bombs = z t }, Cmd.none)
-    Height t ->  ({ settings | height = z t }, Cmd.none)
-    Seed t ->  ({ settings | seed = z t }, Cmd.none)
-    Width t ->  ({ settings | width = z t }, Cmd.none)
-    TimeRequest -> (settings, Task.perform SeedTime Time.now)
+    let
+        z t =
+            Maybe.withDefault 0 <| String.toInt t
+    in
+    case msg of
+        Bombs t ->
+            ( { settings | bombs = z t }, Cmd.none )
+
+        Height t ->
+            ( { settings | height = z t }, Cmd.none )
+
+        Seed t ->
+            ( { settings | seed = z t }, Cmd.none )
+
+        Width t ->
+            ( { settings | width = z t }, Cmd.none )
+
+        TimeRequest ->
+            ( settings, Task.perform SeedTime Time.now )
+
+
 
 -- EVENTS
-onRightClick: msg -> Attribute msg
+
+
+onRightClick : msg -> Attribute msg
 onRightClick message =
-  preventDefaultOn "contextmenu" (Json.map (\m->(m,True)) (Json.succeed message))
+    preventDefaultOn "contextmenu" (Json.map (\m -> ( m, True )) (Json.succeed message))
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  let (flags,bombs) = flagBombCount model.board.squares
-  in div []
-    [ Html.map Change (viewSettings model.settings)
-    , div []
-      [ button [onClick (Set beginner)] [text "Beginner"]
-      , button [onClick (Set intermediate)] [text "Intermediate"]
-      , button [onClick (Set expert)] [text "Expert"]
-      ]
-    , button [ onClick Reset ] [ text <| (String.fromInt flags)++"/"++(String.fromInt bombs)++" Reset" ]
-    , viewBoard model.board
-    ]
+    let
+        ( flags, bombs ) =
+            flagBombCount model.board.squares
+    in
+    div []
+        [ Html.map Change (viewSettings model.settings)
+        , div []
+            [ button [ onClick (Set beginner) ] [ text "Beginner" ]
+            , button [ onClick (Set intermediate) ] [ text "Intermediate" ]
+            , button [ onClick (Set expert) ] [ text "Expert" ]
+            ]
+        , button [ onClick Reset ] [ text <| String.fromInt flags ++ "/" ++ String.fromInt bombs ++ " Reset" ]
+        , viewBoard model.board
+        ]
 
-viewSettings: Settings -> Html SettingsMsg
+
+viewSettings : Settings -> Html SettingsMsg
 viewSettings g =
-  div []
-    [ div [] [text "bombs", input [value (String.fromInt g.bombs), onInput Bombs, type_ "number"][]]
-    , div [] [text "height", input [value (String.fromInt g.height), onInput Height, type_ "number"][]]
-    , div []
-      [text "seed"
-      , input [value (String.fromInt g.seed), onInput Seed, type_ "number"][]
-      , button [onClick TimeRequest][text "🎲"]
-      ]
-    , div [] [text "width", input [value (String.fromInt g.width), onInput Width, type_ "number"][]]
-    ]
+    div []
+        [ div [] [ text "bombs", input [ value (String.fromInt g.bombs), onInput Bombs, type_ "number" ] [] ]
+        , div [] [ text "height", input [ value (String.fromInt g.height), onInput Height, type_ "number" ] [] ]
+        , div []
+            [ text "seed"
+            , input [ value (String.fromInt g.seed), onInput Seed, type_ "number" ] []
+            , button [ onClick TimeRequest ] [ text "🎲" ]
+            ]
+        , div [] [ text "width", input [ value (String.fromInt g.width), onInput Width, type_ "number" ] [] ]
+        ]
 
-viewBoard: Board -> Html Msg
+
+viewBoard : Board -> Html Msg
 viewBoard b =
-  List.range 0 (Board.maxY b)
-  |> List.map (viewRow b)
-  |> div []
+    List.range 0 (Board.maxY b)
+        |> List.map (viewRow b)
+        |> div []
+
 
 viewRow : Board -> Int -> Html Msg
 viewRow b y =
-  List.range 0 (Board.maxX b)
-  |> List.map (viewSquare b y)
-  |> row []
+    List.range 0 (Board.maxX b)
+        |> List.map (viewSquare b y)
+        |> row []
 
-viewSquare: Board -> Int -> Int -> Html Msg
+
+viewSquare : Board -> Int -> Int -> Html Msg
 viewSquare b y x =
-  case Board.square x y b of
-    (Plain,Bomb) -> unswept [ onRightClick (Toggle x y), onClick (Lose x y) ] []
-    (Plain,_) -> unswept [ onRightClick (Toggle x y), onClick (Sweep x y) ] []
-    (Flagged,_) -> unswept [ onRightClick (Toggle x y) ] [text "⚐" ]
-    (Question,_) -> unswept [ onRightClick (Toggle x y) ] [text "?" ]
-    (Exposed,Neighbor 0) -> blank
-    (Exposed,Neighbor 1) -> swept "blue" 1
-    (Exposed,Neighbor 2) -> swept "green" 2
-    (Exposed,Neighbor 3) -> swept "red" 3
-    (Exposed,Neighbor 4) -> swept "purple" 4
-    (Exposed,Neighbor 5) -> swept "maroon" 5
-    (Exposed,Neighbor 6) -> swept "turquoise" 6
-    (Exposed,Neighbor n) -> swept "black" n
-    (Exposed,Bomb) -> lost [] [text "💣"]
+    case Board.square x y b of
+        ( Plain, Bomb ) ->
+            unswept [ onRightClick (Toggle x y), onClick (Lose x y) ] []
+
+        ( Plain, _ ) ->
+            unswept [ onRightClick (Toggle x y), onClick (Sweep x y) ] []
+
+        ( Flagged, _ ) ->
+            unswept [ onRightClick (Toggle x y) ] [ text "⚐" ]
+
+        ( Question, _ ) ->
+            unswept [ onRightClick (Toggle x y) ] [ text "?" ]
+
+        ( Exposed, Neighbor 0 ) ->
+            blank
+
+        ( Exposed, Neighbor 1 ) ->
+            swept "blue" 1
+
+        ( Exposed, Neighbor 2 ) ->
+            swept "green" 2
+
+        ( Exposed, Neighbor 3 ) ->
+            swept "red" 3
+
+        ( Exposed, Neighbor 4 ) ->
+            swept "purple" 4
+
+        ( Exposed, Neighbor 5 ) ->
+            swept "maroon" 5
+
+        ( Exposed, Neighbor 6 ) ->
+            swept "turquoise" 6
+
+        ( Exposed, Neighbor n ) ->
+            swept "black" n
+
+        ( Exposed, Bomb ) ->
+            lost [] [ text "💣" ]
+
+
 
 -- STYLING
 
-row: List (Attribute msg) -> List (Html msg) -> Html msg
-row = div
 
-base: List (Attribute msg) -> List (Html msg) -> Html msg
-base attrs = span <| attrs++
-  [ style "display" "inline-block"
-  , style "width" "20px"
-  , style "height" "20px"
-  , style "border-width" "3px"
-  , style "text-align" "center"
-  , style "vertical-align" "top"
-  , style "font-weight" "bold"
-  ]
+row : List (Attribute msg) -> List (Html msg) -> Html msg
+row =
+    div
 
-unswept: List (Attribute msg) -> List (Html msg) -> Html msg
-unswept attrs = base <| attrs++
-  [ style "background-color" "gray"
-  , style "border-style" "outset"
-  ]
 
-blank: Html msg
-blank = base
-  [ style "background-color" "lightgray"
-  , style "border-style" "solid"
-  , style "border-color" "gray"
-  ]
-  []
-swept: String -> Int -> Html msg
-swept c n = base
-  [ style "background-color" "lightgray"
-  , style "border-style" "solid"
-  , style "border-color" "gray"
-  , style "color" c
-  ]
-  [text <| String.fromInt n]
+base : List (Attribute msg) -> List (Html msg) -> Html msg
+base attrs =
+    span <|
+        attrs
+            ++ [ style "display" "inline-block"
+               , style "width" "20px"
+               , style "height" "20px"
+               , style "border-width" "3px"
+               , style "text-align" "center"
+               , style "vertical-align" "top"
+               , style "font-weight" "bold"
+               ]
 
-lost: List (Attribute msg) -> List (Html msg) -> Html msg
-lost attrs = base <| attrs++
-  [ style "background-color" "red"
-  , style "border-style" "solid"
-  , style "border-color" "red"
-  ]
 
+unswept : List (Attribute msg) -> List (Html msg) -> Html msg
+unswept attrs =
+    base <|
+        attrs
+            ++ [ style "background-color" "gray"
+               , style "border-style" "outset"
+               ]
+
+
+blank : Html msg
+blank =
+    base
+        [ style "background-color" "lightgray"
+        , style "border-style" "solid"
+        , style "border-color" "gray"
+        ]
+        []
+
+
+swept : String -> Int -> Html msg
+swept c n =
+    base
+        [ style "background-color" "lightgray"
+        , style "border-style" "solid"
+        , style "border-color" "gray"
+        , style "color" c
+        ]
+        [ text <| String.fromInt n ]
+
+
+lost : List (Attribute msg) -> List (Html msg) -> Html msg
+lost attrs =
+    base <|
+        attrs
+            ++ [ style "background-color" "red"
+               , style "border-style" "solid"
+               , style "border-color" "red"
+               ]
